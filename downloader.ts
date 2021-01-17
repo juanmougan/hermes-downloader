9e4f7dc54a65e33a268ec7ff6b73d80230cef8eb
Juan Manuel Mougan <juanmougan@gmail.com>
2021-01-17 16:13:18 -0300

    Guarding against large downloads

--- downloader.ts
+++ downloader.ts
@@ -1,7 +1,17 @@
 export class Downloader {
   public async downloadVideos(videos: string[]): Promise<void> {
       const outputDirName = Downloader.createOutputDirectory()
-      videos.forEach(v => this.downloadVideo(v, outputDirName))
+      const config = require('config')
+      if (videos.length > config.get('hermes.videosThreshold')) {
+        // Dry run
+        const commandOutput = config.get('hermes.commandOutput')
+        console.log(`Too many videos! Please download them manually, get the commands from ${commandOutput}`)
+        const fs = require('fs');
+        const fileConsole = new console.Console(fs.createWriteStream(`./${commandOutput}`));
+        videos.forEach(v => this.prepareCommand(v, outputDirName).then(v => fileConsole.log(v)))
+      } else {
+        videos.forEach(v => this.downloadVideo(v, outputDirName))
+      }
   }
 
   private static createOutputDirectory() {
@@ -12,15 +22,18 @@ export class Downloader {
     }
     return outputDirName
   }
-  
-  private async downloadVideo(video, dir): Promise<void> {
-      const safariEmail = process.env.SAFARI_EMAIL
+
+  private async prepareCommand(video: string, dir: string) {
+    const safariEmail = process.env.SAFARI_EMAIL
       const password = process.env.SAFARI_PASSWORD
       const urlParts = video.split('/')
       const videoName = `${urlParts[urlParts.length - 1]}.mp4`
       const output = `${dir}/${videoName}`
-      console.log(`Downloading ${video} to ${output}`)
-      const command = `youtube-dl -u ${safariEmail} -p ${password} --verbose --output ${dir}/${videoName} ${video}`
+      return `youtube-dl -u ${safariEmail} -p ${password} --verbose --output ${output} ${video}`
+  }
+  
+  private async downloadVideo(video, dir): Promise<void> {
+      const command = this.prepareCommand(video, dir)
       const { exec } = require("child_process");
       exec(command, (error, stdout, stderr) => {
         if (error) {
