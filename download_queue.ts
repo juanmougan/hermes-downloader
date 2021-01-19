4328363fa896e5344b88e0a7d1eec261ba410332
Juan Manuel Mougan <juanmougan@gmail.com>
2021-01-19 00:01:30 -0300

    This implementation copes with ALMOST 400 files

--- download_queue.ts
+++ download_queue.ts
@@ -3,11 +3,18 @@ import { Worker, QueueScheduler, Queue, Job, QueueEvents } from 'bullmq';
 export class DownloadQueue {
   downloadQueue: Queue<any, any, string>
   scheduler: QueueScheduler
-  config: any;
-  workers: number;
+  config: any
+  workers: number
+  lastJob: boolean
   
   constructor(queueName: string) {
-    this.downloadQueue = new Queue(queueName);
+    const queueOptions = { defaultJobOptions: {
+      removeOnComplete: true
+    }}
+    this.downloadQueue = new Queue(queueName, queueOptions);
+    // The docs say:
+    // Jobs that get rate limited will actually end as delayed jobs, so you need at least one QueueScheduler 
+    // somewhere in your deployment so that jobs are put back to the wait status.
     this.scheduler = new QueueScheduler(this.downloadQueue.name);
     this.config = require('config')
   }
@@ -24,44 +31,32 @@ export class DownloadQueue {
     urls.forEach(url => this.downloadQueue.add(url, { url }, queueConfig))
   }
 
-  private async logJobCount() {
-    console.log("Completed: ", await this.downloadQueue.getCompletedCount())
-    console.log("Delayed: ", await this.downloadQueue.getDelayedCount())
-    console.log("Failed: ", await this.downloadQueue.getFailedCount())
-  }
-
   public async processJobs(jobImpl: (url: string) => Promise<string>) {    
-    const workers = []
-    for (let i = 0; i < this.workers; i++) {
-      //const worker = new Worker('my-queue', null, { lockDuration: 60000 });
+    for (let i = 0; i < this.config.get('hermes.maxDownloadsPerTimeThreshold'); i++) {
       const worker = new Worker(this.downloadQueue.name, async job => jobImpl(job.data.url), {
-        limiter: {
-          max: this.config.get('hermes.maxDownloadsPerTimeThreshold'),
-          duration: this.config.get('hermes.timeThreshold')
-        },
         lockDuration: this.config.get('hermes.lockDuration')
       });
       // Check for errors
       worker.on("failed", (job: Job, failedReason: string) => {
         console.log(`${job.id} has failed with ${failedReason}`, job.data);
-        this.logJobCount()
       });
       // Log success
-      worker.on("completed", (job: Job, returnValue: string) => {
+      worker.on("completed", async (job: Job, returnValue: string) => {
         console.log(`Completed job (${job.id})`)
-        this.logJobCount()
+        // If there are no more jobs, exit OK
+        // TODO this is quite hacky, improve
+        const jobsToBeProcessed = await this.downloadQueue.count()
+        if (jobsToBeProcessed === 0) {
+          if (this.lastJob) {
+            // I've finished processing, exit
+            return
+          } else {
+            // Mark that the next sucessful job should be the last one
+            this.lastJob = true
+          }
+        }
       });
-      workers.push(worker)
     }
-    // Listen to events
-    const queueEvents = new QueueEvents(this.downloadQueue.name);
-    queueEvents.on('completed', jobId => {
-      // console.log('Job completed with id: ', jobId);
-      console.log('Job completed');
-    });
-    queueEvents.on('failed', (jobId, err) => {
-      console.error('Error running the job with id: ', jobId, err);
-    });
   }
 
   public async shutdown() {
--- downloader.ts
+++ downloader.ts
@@ -1,4 +1,3 @@
-import { Job, Queue, QueueScheduler } from 'bullmq';
 import { DownloadQueue } from './download_queue';
 
 export class Downloader {
@@ -17,18 +16,17 @@ export class Downloader {
   }
 
   // TODO remove duplication, there are 2 methods doing the same
+  // TODO move the command preparation somewhere else
   private async performDownload(url: string, outputDirName: string): Promise<string> {
     const safariEmail = process.env.SAFARI_EMAIL
     const password = process.env.SAFARI_PASSWORD
     const urlParts = url.split('/')
     const videoName = `${urlParts[urlParts.length - 1]}.mp4`
     const output = `${outputDirName}/${videoName}`
-    // console.log(`Downloading ${video} to ${output}`)
-    // const command = `youtube-dl -u ${safariEmail} -p ${password} --verbose --output ${output} ${url} > /dev/null`
     const command = 'youtube-dl'
     const args = ['-u', safariEmail, '-p', password, '--output', output, url]
 
-    return await this.execProcess(command, args)
+    return this.execProcess(command, args)
   }
 
   private async execProcess(command: string, args: Array<string>): Promise<string> {
@@ -46,15 +44,6 @@ export class Downloader {
     })
   }
 
-  private async enqueueJobs(videos: string[], videosQueue: Queue) {
-    const jobIds = []
-    for(let i = 0; i < videos.length; i++) {
-      const jobId = `${i}-${videos[i]}`
-      jobIds.push(jobId)
-      await videosQueue.add(jobId, { url: videos[i]});
-    }
-  }
-
   private static createOutputDirectory() {
     const fs = require('fs')
     const outputDirName = `output/${Date.now()}`
@@ -64,13 +53,14 @@ export class Downloader {
     return outputDirName
   }
 
+  // TODO remove duplication, there are 2 methods doing the same
+  // TODO move the command preparation somewhere else
   private async downloadVideo(video, dir) {
       const safariEmail = process.env.SAFARI_EMAIL
       const password = process.env.SAFARI_PASSWORD
       const urlParts = video.split('/')
       const videoName = `${urlParts[urlParts.length - 1]}.mp4`
       const output = `${dir}/${videoName}`
-      // console.log(`Downloading ${video} to ${output}`)
       const command = `youtube-dl -u ${safariEmail} -p ${password} --verbose --output ${output} ${video}`
       console.log("Download: ", command)
       const { exec } = require("child_process");
